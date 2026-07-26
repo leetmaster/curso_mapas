74d7b6c2c8d1bc55f0c3cb16c33e5ee28ac49b0b
Ruben Garcia <leetmaster@users.noreply.github.com>
2026-07-26 11:41:55 -0600

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,55 +1,75 @@
 import pandas as pd
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+from pathlib import Path
 import re
 import random
 import folium
-from folium import FeatureGroup, LayerControl, Map, Marker
 
-def transformar_coordenadas(df):
-    final_coords = []
-    for wkt in list(df['WKT']):
-        coords = re.sub(r'[a-zA-Z()]', '', wkt).strip().split(',')
-        final_coords.append(list(map(lambda x: (float(x.split(' ')[1]), float(x.split(' ')[0])), coords)))
-    return final_coords
+from shapely import wkt
+from shapely.geometry import mapping
+from shapely.geometry import Point, LineString
 
+THIS_FOLDER = Path(__file__).parent.resolve()
 
-def capa(df, coordenadas, tipo, nombre_capa, mostrar=False):
-    feature_group = FeatureGroup(name=nombre_capa, show=mostrar)  # overlay=False
+def capa(df, nombre_capa, mostrar=False):
 
-    for index, coords in enumerate(coordenadas):
+    feature_group = folium.FeatureGroup(name=nombre_capa, show=mostrar)
 
-        if tipo == 'Punto':
-            color = random.choices(
-                ['gray', 'darkpurple', 'beige', 'lightgreen', 'lightblue', 'red', 'green', 'purple', 'darkred', 'black',
-                 'cadetblue', 'blue', 'orange', 'pink', 'lightgray', 'darkgreen', 'darkblue', 'lightred'])[0]
+    colores = [
+        'gray','darkpurple','beige','lightgreen','lightblue','red',
+        'green','purple','darkred','black','cadetblue','blue',
+        'orange','pink','lightgray','darkgreen','darkblue','lightred'
+    ]
+    print(nombre_capa)
+    for _, row in df.iterrows():
+
+        geom = wkt.loads(row["WKT"])
+        popup_html = folium.Popup(popup(row))
+        
+        if isinstance(geom, Point):
+
             folium.Marker(
-                location=[coords[0][0], coords[0][1]],
-                popup=folium.Popup(popup(df.loc[index,:].to_dict())), #min_width=850, max_width=850
-                icon=folium.Icon(color=color, icon='building', prefix='fa'), ).add_to(feature_group)
-        if tipo == 'Poligono':
-            folium.Polygon(coords,
-                           weight=2,
-                           color="red",
-                           fill_color="yellow",
-                           fill_opacity=0.3,
-                           popup=f'Manzana {index}').add_to(feature_group)  # folium.Popup(popup(df.loc[index,:].to_dict()))).add_to(feature_group)
-        if tipo == 'Linea':
-            folium.PolyLine(coords,
-                            weight=2,
-                            color="red",
-                            fill_color="yellow",
-                            fill_opacity=0.3,
-                            popup=folium.Popup(popup(df.loc[index,:].to_dict()))).add_to(feature_group)
+                location=(geom.y, geom.x),
+                popup=popup_html,
+                icon=folium.Icon(
+                    color=random.choice(colores),
+                    icon="coffee",
+                    prefix="fa"
+                ),
+            ).add_to(feature_group)
+
+        elif isinstance(geom, (LineString)):
+            folium.GeoJson(
+                    mapping(geom),
+                    popup=popup_html,
+                    style_function=lambda _: {
+                        "color": "green",
+                        "weight": 2,
+                        "fillColor": "green",
+                        "fillOpacity": 0.2,
+                    },
+                ).add_to(feature_group)
+
+        else:
+            folium.GeoJson(
+                mapping(geom),
+                popup=popup_html,
+                style_function=lambda _: {
+                    "color": "blue",
+                    "weight": 1,
+                    "fillColor": "gray",
+                    "fillOpacity": 0.2,
+                },
+            ).add_to(feature_group)
 
     return feature_group
-
 
 def unir_capas(m, capas):
     for capa in capas:
         capa.add_to(m)
-    LayerControl().add_to(m)
-    m.save('templates/mapa_generado.html')
+    folium.LayerControl().add_to(m)
+    m.save(THIS_FOLDER / 'templates/mapa_generado.html')
     return m
 
 def popup(row):
@@ -59,28 +79,24 @@ def popup(row):
             cad += f'<b>{col.upper()}:</b> {value.upper() if type(value) == str else str(value)}<br>\n'
         return cad
 
-def crear_capa(database, tabla, tipo, nombre_capa, mostrar = False):
+def crear_capa(database, tabla, nombre_capa, mostrar=False):
 
     if isinstance(database, dict):
-        database['table_name'] = tabla 
+        database["table_name"] = tabla
         df = obtener_datos(database)
-    else: 
+    else:
         df = pd.read_csv(database)
-    coordenadas = transformar_coordenadas(df)
-    return capa(df, coordenadas, tipo, nombre_capa, mostrar)
+
+    return capa(df, nombre_capa, mostrar)
 
 
 def obtener_datos(database):
+    engine = create_engine(f"mysql+pymysql://{database['user']}:{database['password']}@{database['host']}/{database['db_name']}")
 
-    engine = create_engine(f"postgresql+psycopg2://{database['user']}:{database['password']}@{database['host']}:{database['port']}/{database['db_name']}")
-    if not inspect(engine).has_table(database['table_name'], schema="public"):
-        return False
     return pd.read_sql_query(f'SELECT * FROM {database["table_name"]}', engine)
 
-def AddDatosTabla(data, database):
 
-    engine = create_engine(f"postgresql+psycopg2://{database['user']}:{database['password']}@{database['host']}:{database['port']}/{database['db_name']}")
-    if not database_exists(engine.url):
-        create_database(engine.url)
+def crear_tabla(data, database):
+    engine = create_engine(f"mysql+pymysql://{database['user']}:{database['password']}@{database['host']}/{database['db_name']}")
+
     data.to_sql(database['table_name'], engine, if_exists= 'append', index=False)
-    
